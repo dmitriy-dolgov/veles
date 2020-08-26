92ac94fbbd491242f24577a88f66b7bab40585bd
dmitriy-dolgov <TwilightTower@mail.ru>
2020-08-26 10:03:33 +0300

    background and menu stuff

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="fonts/icomoon.css">
-    <link rel="stylesheet" href="main.css?v=0.3">
+    <link rel="stylesheet" href="main.css?v=0.4">
 
 </head>
 <body>
