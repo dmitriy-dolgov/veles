b1949dd57dc95bf707c01015e3fe0646cc7762c8
dmitriy-dolgov <TwilightTower@mail.ru>
2020-08-26 09:00:37 +0300

    border for order

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="fonts/icomoon.css">
-    <link rel="stylesheet" href="main.css?v=0.2">
+    <link rel="stylesheet" href="main.css?v=0.3">
 
 </head>
 <body>
